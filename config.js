9955a50cdf1f903054be93b31ab08aec34d22a39
belskiyroman <belskiyroman@gmail.com>
2014-12-08 02:21:44 +0200

    change port in config

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ var path = require('path');
 
 module.exports = {
 	_pablic: path.join(__dirname, 'htdocs'),
-	port: 3000,
+	port: process.env.PORT || 5000,
     db: path.join(__dirname, 'db', 'db.json')
 };
